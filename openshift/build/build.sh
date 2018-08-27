975346041f5735f7f8255f4dd98b4143ae2b2ddb
Andrew DeMaria <lostonamountain@gmail.com>
2018-08-27 16:45:15 -0400

    Added build

---
+++ openshift/build/build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -eE -o pipefail
+
+OC_ARGS=""
+if [[ -n "$KUBERNETES_MASTER" ]];
+then
+  OC_ARGS="--token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token) --namespace=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace) --certificate-authority=/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+fi
+
+function oc() {
+  command oc $OC_ARGS "$@"
+  return $?
+}
+
+dir=$(dirname "$(readlink -f "$0")")
+
+if [[ -n "$OCP_ENV" && -f $dir/../env/${OCP_ENV}.env ]];
+then
+  set -a
+  . $dir/../env/${OCP_ENV}.env
+  set +a
+fi
+
+oc apply -f $dir/build.yml
+oc start-build mule-ee
